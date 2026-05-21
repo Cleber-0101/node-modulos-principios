1fc735c72ea7656f32aaf71ac9139671596a5827
Cleber-0101 <cleberbatista99@hotmail.com>
2026-05-20 23:40:15 -0300

    Criando funcionalidade de ler e escrever um arquivo.txt com node, aplicando principios Npm

---
+++ arquiteturaNodeJs/node.js
@@ -0,0 +1,20 @@
+let {readFile, writeFile} = require('fs')
+
+
+// lendo o arquivo 
+readFile("arquivo.txt", "utf-8", (error, texto) =>{
+    if(error){
+        throw error
+    }else{
+        console.log(texto)
+    }
+})
+
+// escrevendo dentro do arquivo 
+writeFile("arquivo.txt", "Estou escrevendo dentro do arquivo.txt", (error) =>{
+    if(error){
+        throw error
+    }else{
+        console.log("deu certo.....")
+    }
+})
