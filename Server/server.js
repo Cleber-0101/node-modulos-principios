13528a32016de9b81347a38bde56946fce4d0328
Cleber-0101 <cleberbatista99@hotmail.com>
2026-05-22 05:41:10 -0300

    Criando servidor de forma padronizada

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,11 @@
-const { createServer } = require('https')
+const { createServer } = require('http')
 
-const server = createServer((req,res) => {
-    
+const server = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write('<h1>Ola mundo teste </h1>')
+    res.end()
+})
+
+server.listen(8000, () => {
+    console.log('Ouvindo a porta 8000')
 })
